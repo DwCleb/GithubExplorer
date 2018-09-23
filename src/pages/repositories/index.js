49f9c88efbcd0c4e67d5a9d729f3c87a443d2f2a
Cleber Junior <CleberJr@Clebers-MacBook-Pro.local>
2018-09-23 20:52:23 -0300

    List of Repo

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { View } from 'react-native';
+import {
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
+import api from 'services/api';
+
+import styles from './styles';
 
 export default class Repositories extends Component {
   static navigationOptions = {
@@ -10,7 +20,45 @@ export default class Repositories extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={22} color={ tintColor }/>,
   };
 
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  }
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: false });
+    const username = await AsyncStorage.getItem('@GithubExplorer:username');
+    const response = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data: response.data, loading: false });
+  }
+
+  renderListItem = ({ item }) => (
+    <Text>{item.full_name}</Text>
+  )
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+    />
+  )
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        { 
+          this.state.loading
+            ? <ActivityIndicator style={styles.loading} />
+            : this.renderList()
+        }
+      </View>
+    );
   }
 }
